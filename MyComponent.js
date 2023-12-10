c42fe2894458cbfab883e71fd3826447ff7ba0d4
imZaibaF <101112702+imZaibaF@users.noreply.github.com>
2023-12-10 22:40:08 +0530

    Add files via upload

---
+++ MyComponent.js
@@ -0,0 +1,528 @@
+import * as React from "react";
+
+import Layout from "../components/Layout";
+
+const MyComponent = () => {
+  function MyComponent(props) {
+    return (
+      <Div>
+        <Div2>
+          <Div3>
+            <Img loading="lazy" src="pic1.jpg" />
+            <Div4>
+              <Div5>
+                <Div6>Career</Div6>
+                <Div7>Blogs</Div7>
+                <Div8>Leaderboard</Div8>
+                <Div9>Fees</Div9>
+              </Div5>
+              <Div10>Trade Now</Div10>
+            </Div4>
+          </Div3>
+        </Div2>
+        <Div11>
+          <Div12>
+            <Div13>
+              <span style="color: rgba(255,255,255,1);">
+                It’s time to trade,
+                <br />
+                the{" "}
+              </span>
+              future.
+            </Div13>
+            <Div14>
+              Trade BTC, ETH Futures with 125x leverage and earn rewards.
+            </Div14>
+            <Img2 loading="lazy" srcSet="..." />
+          </Div12>
+        </Div11>
+        <Div15>
+          <Div16>
+            <Column>
+              <Div17>
+                <Div18>00%</Div18>
+                <Div19>Conversion Fee </Div19>
+              </Div17>
+            </Column>
+            <Column2>
+              <Div20>
+                <Div21>500 Mn+</Div21>
+                <Div22>Lifetime Volume Traded</Div22>
+              </Div20>
+            </Column2>
+            <Column3>
+              <Div23>
+                <Div24>250+</Div24>
+                <Div25>Total Tradable Pairs</Div25>
+              </Div23>
+            </Column3>
+            <Column4>
+              <Div26>
+                <Div27>15,000+</Div27>
+                <Div28>Traders</Div28>
+              </Div26>
+            </Column4>
+          </Div16>
+        </Div15>
+        <Div29>
+          <Div30>
+            <Img3 loading="lazy" src="pic2" />
+            <Img4 loading="lazy" src="pic" />
+            <Img5 loading="lazy" src="pic" />
+          </Div30>
+        </Div29>
+        <Div31>
+          <span style="color: rgba(255,255,255,1);">Trade More. </span>
+          <span style="color: rgba(235,255,37,1);">Pay Less.</span>
+        </Div31>
+        <Div32>Our low Fees Supercharge Your Profits</Div32>
+        <Div33>
+          <Div34>
+            <Div35>
+              <Column5>
+                <Div36>
+                  <Div37>
+                    <Img6 loading="lazy" src="pic" />
+                    <Img7 loading="lazy" srcSet="..." />
+                    <Div38>0.025</Div38>
+                    <Div39>Maker Fees</Div39>
+                  </Div37>
+                  <Div40>0.07</Div40>
+                  <Div41>Taker Fees</Div41>
+                </Div36>
+              </Column5>
+              <Column6>
+                <Div42>
+                  <Img8 loading="lazy" srcSet="..." />
+                </Div42>
+              </Column6>
+              <Column7>
+                <Div43>
+                  <Div44>
+                    <Img9 loading="lazy" src="\pic" />
+                    <Img10 loading="lazy" srcSet="..." />
+                    <Div45>0.02</Div45>
+                    <Div46>Maker Fees</Div46>
+                  </Div44>
+                  <Div47>0.05</Div47>
+                  <Div48>Taker Fees</Div48>
+                </Div43>
+              </Column7>
+            </Div35>
+          </Div34>
+        </Div33>
+        <Div49>
+          <Div50>
+            <Div51>
+              <span style="color: rgba(255,255,255,1);">
+                Explore the Markets
+                <br />
+              </span>
+              <span style="color: rgba(255,255,255,1);">like it is your </span>
+              <span style="color: rgba(235,255,37,1);">Playground.</span>
+            </Div51>
+            <Div52>
+              Search for your favorite coins and stay ahead of the market
+            </Div52>
+            <Img11 loading="lazy" srcSet="..." />
+            <Div53>Explore Markets</Div53>
+          </Div50>
+        </Div49>
+        <Div54>
+          <Div55>
+            <Div56>
+              <span style="color: rgba(235,255,37,1);">Unlock</span>
+              <span style="color: rgba(212,249,56,1);"> </span>
+              <span style="color: rgba(255,255,255,1);">New Frontiers.</span>
+            </Div56>
+            <Div57>
+              Are you a stock trader looking for new opportunities to make
+              money? We got you covered!
+            </Div57>
+            <Div58>
+              <Div59>
+                <Column8>
+                  <Div60>
+                    Same
+                    <br />
+                    Strategies
+                  </Div60>
+                </Column8>
+                <Column9>
+                  <Div61>
+                    Same
+                    <br />
+                    Indicators
+                  </Div61>
+                </Column9>
+                <Column10>
+                  <Div62>
+                    Better
+                    <br />
+                    Leverage
+                  </Div62>
+                </Column10>
+                <Column11>
+                  <Div63>
+                    24x7
+                    <br />
+                    Trading
+                  </Div63>
+                </Column11>
+              </Div59>
+            </Div58>
+            <Img12 loading="lazy" srcSet="..." />
+          </Div55>
+        </Div54>
+        <Div64>
+          <Div65>
+            <Div66>
+              <Div67>
+                <Div68>Start Small. Earn Big.</Div68>
+                <Div69>
+                  Deposity a minimum of 1000 and get a Deposit bonus +
+                  dedicagted relationship manager
+                </Div69>
+              </Div67>
+            </Div66>
+            <Div70>
+              <Div71>
+                <Column12>
+                  <Img13 loading="lazy" srcSet="..." />
+                </Column12>
+                <Column13>
+                  <Img14 loading="lazy" srcSet="..." />
+                </Column13>
+              </Div71>
+            </Div70>
+          </Div65>
+        </Div64>
+        <Div72>
+          <span style="color: rgba(255,255,255,1);">
+            Derivates made simple
+            <br />
+            in{" "}
+          </span>
+          <span style="color: rgba(235,255,37,1);">3 Easy</span>
+          <span style="color: rgba(255,255,255,1);"> Steps</span>
+        </Div72>
+        <Div73>
+          <Div74>
+            <Div75>
+              <Column14>
+                <Img15 loading="lazy" srcSet="..." />
+              </Column14>
+              <Column15>
+                <Div76>
+                  <Img16 loading="lazy" srcSet="..." />
+                  <Div77>Create an Account</Div77>
+                  <Div78>
+                    Register & Complete your Verification in less than 2 minutes
+                  </Div78>
+                  <Div79>Trade Now</Div79>
+                </Div76>
+              </Column15>
+            </Div75>
+          </Div74>
+        </Div73>
+        <Img17 loading="lazy" src="pic" />
+        <Div80>
+          <Div81>
+            <Div82>
+              <Column16>
+                <Img18 loading="lazy" srcSet="..." />
+              </Column16>
+              <Column17>
+                <Div83>
+                  <Img19 loading="lazy" src="pic" />
+                  <Div84>Deposit Funds</Div84>
+                  <Div85>
+                    Add funds quickly using a variety of payment methods
+                  </Div85>
+                  <Div86>Trade Now</Div86>
+                </Div83>
+              </Column17>
+            </Div82>
+          </Div81>
+        </Div80>
+        <Img20 loading="lazy" src="pic" />
+        <Div87>
+          <Div88>
+            <Div89>
+              <Column18>
+                <Img21 loading="lazy" srcSet="..." />
+              </Column18>
+              <Column19>
+                <Div90>
+                  <Img22 loading="lazy" srcSet="..." />
+                  <Div91>Become a Trader</Div91>
+                  <Div92>Choose Your Trading Pair & Trade Seamlessly</Div92>
+                  <Div93>Trade Now</Div93>
+                </Div90>
+              </Column19>
+            </Div89>
+          </Div88>
+        </Div87>
+        <Div94>Backed by the Best.</Div94>
+        <Div95>
+          <Div96>
+            <Div97>
+              <Div98>
+                <Div99>
+                  <Column20>
+                    <Img23 loading="lazy" srcSet="..." />
+                  </Column20>
+                  <Column21>
+                    <Img24 loading="lazy" srcSet="..." />
+                  </Column21>
+                  <Column22>
+                    <Img25 loading="lazy" srcSet="..." />
+                  </Column22>
+                  <Column23>
+                    <Img26 loading="lazy" srcSet="..." />
+                  </Column23>
+                </Div99>
+              </Div98>
+              <Div100>
+                <Div101>
+                  <Column24>
+                    <Img27 loading="lazy" srcSet="..." />
+                  </Column24>
+                  <Column25>
+                    <Img28 loading="lazy" srcSet="..." />
+                  </Column25>
+                  <Column26>
+                    <Img29 loading="lazy" srcSet="..." />
+                  </Column26>
+                </Div101>
+              </Div100>
+            </Div97>
+            <Div102>
+              <Div103>
+                <Div104>
+                  <Img30 loading="lazy" srcSet="..." />
+                  <Div105>
+                    Utsav Somani
+                    <br />
+                  </Div105>
+                </Div104>
+                <Div106>
+                  <Img31 loading="lazy" srcSet="..." />
+                  <Div107>
+                    <span style="font-family: Neurial Grotesk, sans-serif;font-weight: 700;color: rgba(255,255,255,1);">
+                      Aditya Nagarsheth
+                      <br />
+                    </span>
+                    <span style="font-family: Neurial Grotesk, sans-serif;font-weight: 500;color: rgba(128,128,144,1);">
+                      Perpetual Value Partners
+                    </span>
+                  </Div107>
+                </Div106>
+                <Div108>
+                  <Img32 loading="lazy" srcSet="..." />
+                  <Div109>
+                    Gokul Rajaram
+                    <br />
+                  </Div109>
+                </Div108>
+                <Div110>
+                  <Img33 loading="lazy" srcSet="..." />
+                  <Div111>
+                    <span style="font-family: Neurial Grotesk, sans-serif;font-weight: 700;color: rgba(255,255,255,1);">
+                      Sajid Rehman
+                      <br />
+                    </span>
+                    <span style="font-family: Neurial Grotesk, sans-serif;font-weight: 500;color: rgba(128,128,144,1);">
+                      My Asia VC
+                    </span>
+                  </Div111>
+                </Div110>
+                <Div112>
+                  <Img34 loading="lazy" srcSet="..." />
+                  <Div113>
+                    <span style="font-family: Neurial Grotesk, sans-serif;font-weight: 700;color: rgba(255,255,255,1);">
+                      Arjun Sethi
+                      <br />
+                    </span>
+                    <span style="font-family: Neurial Grotesk, sans-serif;font-weight: 500;color: rgba(128,128,144,1);">
+                      Tribe Capital
+                    </span>
+                  </Div113>
+                </Div112>
+              </Div103>
+              <Div114>
+                <Div115>
+                  <div>
+                    <Div116>
+                      <Img35 loading="lazy" srcSet="..." />
+                      <Div117>Kunal Shah</Div117>
+                    </Div116>
+                  </div>
+                  <Column28>
+                    <Div118>
+                      <Img36 loading="lazy" srcSet="..." />
+                      <Div119>
+                        <span style="font-family: Neurial Grotesk, sans-serif;font-weight: 700;color: rgba(255,255,255,1);">
+                          Sandeep Nailawal
+                          <br />
+                        </span>
+                        <span style="font-family: Neurial Grotesk, sans-serif;font-weight: 500;color: rgba(128,128,144,1);">
+                          Polygon Labs
+                        </span>
+                      </Div119>
+                    </Div118>
+                  </Column28>
+                  <Column29>
+                    <Div120>
+                      <Img37 loading="lazy" srcSet="..." />
+                      <Div121>Karn Vivek Nagpal</Div121>
+                    </Div120>
+                  </Column29>
+                </Div115>
+              </Div114>
+            </Div102>
+          </Div96>
+        </Div95>
+        <Div122>
+          <Div123>
+            <Div124>
+              <Div125>
+                <Div126>Don’t take our word for it.</Div126>
+                <Div127>
+                  Hear it from our expert community of traders who have made
+                  insane amounts in a short amount of time
+                </Div127>
+              </Div125>
+            </Div124>
+            <Div128>
+              <Div129>
+                <Column30>
+                  <Img38 loading="lazy" srcSet="..." />
+                </Column30>
+                <Column31>
+                  <Img39 loading="lazy" srcSet="..." />
+                </Column31>
+                <Column32>
+                  <Img40 loading="lazy" srcSet="..." />
+                </Column32>
+              </Div129>
+            </Div128>
+          </Div123>
+        </Div122>
+        <Div130>
+          <Div131>
+            <Img41 loading="lazy" srcSet="..." />
+            <Div132>
+              <span style="color: rgba(255,255,255,1);">Earn Money. </span>
+              <span style="color: rgba(235,255,37,1);">The Easy Way.</span>
+            </Div132>
+            <Div133>
+              No Complexity of Trading Fee, generate volume and win
+            </Div133>
+            <Div134>
+              <Div135>
+                <Column33>
+                  <Img42 loading="lazy" srcSet="..." />
+                </Column33>
+                <Column34>
+                  <Img43 loading="lazy" srcSet="..." />
+                </Column34>
+                <Column35>
+                  <Img44 loading="lazy" srcSet="..." />
+                </Column35>
+              </Div135>
+            </Div134>
+            <Div136>start earning now</Div136>
+          </Div131>
+        </Div130>
+        <Div137>
+          <Img45 loading="lazy" srcSet="..." />
+          <Div138>
+            <Div139>
+              <span style="color: rgba(255,255,255,1);">Trade Together. </span>
+              <span style="color: rgba(235,255,37,1);">Thrive Together.</span>
+            </Div139>
+            <Div140>Join the fun-filled community on our platform.</Div140>
+            <Div141>
+              <Div142>
+                <div>
+                  <Div143>
+                    <Div144>10,000+</Div144>
+                    <Div145>Traders</Div145>
+                  </Div143>
+                </div>
+                <Column37>
+                  <Div146>
+                    <Div147>100Mn</Div147>
+                    <Div148>Daily Volume Traded</Div148>
+                  </Div146>
+                </Column37>
+                <Column38>
+                  <Div149>
+                    <Div150>Daily</Div150>
+                    <Div151>Trade Analysis</Div151>
+                  </Div149>
+                </Column38>
+                <Column39>
+                  <Div152>
+                    <Div153>
+                      <Div154>Live</Div154>
+                      <Div155>Signals</Div155>
+                    </Div153>
+                  </Div152>
+                </Column39>
+              </Div142>
+            </Div141>
+            <Div156>Join community</Div156>
+          </Div138>
+        </Div137>
+        <Div157>
+          <Div158>
+            <Div159>
+              <span style="color: rgba(255,255,255,1);">Meet the </span>
+              <span style="color: rgba(235,255,37,1);">Visionaries</span>
+              <span style="color: rgba(255,255,255,1);"> behind Density.</span>
+            </Div159>
+            <Div160>
+              <Div161>
+                <Column40>
+                  <Img46 loading="lazy" srcSet="..." />
+                </Column40>
+                <Column41>
+                  <Img47 loading="lazy" srcSet="..." />
+                </Column41>
+                <Column42>
+                  <Img48 loading="lazy" srcSet="..." />
+                </Column42>
+              </Div161>
+            </Div160>
+          </Div158>
+        </Div157>
+        <Div162>Get started now.</Div162>
+        <Div163>Start Trading!</Div163>
+        <Div164>
+          <Div165>
+            <Img49 loading="lazy" src="pic" />
+            <Div166>
+              <Div167>Blog</Div167>
+              <Div168>Fees</Div168>
+              <Div169>Leaderboard</Div169>
+              <Div170>Careers</Div170>
+              <Div171>Contact Us</Div171>
+              <Div172>Privacy Policy</Div172>
+            </Div166>
+            <Div173>
+              Lörem ipsum od ohet dilogi. Bell trabel, samuligt, ohöbel utom
+              diska. Jinesade bel när feras redorade i belogi. FAR paratyp i
+              muvåning, och pesask vyfisat. Viktiga poddradio har un mad och
+              inde.{" "}
+            </Div173>
+            <Img50 loading="lazy" src="pic" />
+          </Div165>
+        </Div164>
+      </Div>
+    );
+  }
+};
+
+export default MyComponent;
